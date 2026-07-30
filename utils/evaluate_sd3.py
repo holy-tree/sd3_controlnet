00bf73c143ad63fa3fbf1d526b4054b1545940a4
hollow_tree <2190964012@qq.com>
2026-07-30 11:01:37 +0800

    [fix] 证明controlnet强度与指标效果正相关，现在添加dump脚本检查vae是否就已经有格子条纹

--- utils/evaluate_sd3.py
+++ utils/evaluate_sd3.py
@@ -407,7 +407,6 @@ def evaluate(args_config: dict):
                         height=args_config["resolution"],
                         width=args_config["resolution"],
                         num_images_per_prompt=1,
-                        controlnet_conditioning_scale=0.5,
                     ).images
                 infer_time_total = time.time() - t0
                 infer_time_avg = infer_time_total / B
